256656bea3e4603eaf7e6398dfbad60d1e12c2ab
wanyu <594564264@qq.com>
2023-11-12 23:00:55 +0800

    [modify] 打包规范修改

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM debian:stable-20231030-slim
-MAINTAINER wanyu
-ADD target/app /opt
-EXPOSE 8080
-ENTRYPOINT ["/opt/app"]
--- opt.bat
+++ opt.bat
@@ -1,12 +1,17 @@
 docker run --rm -it -v /d/software/maven/mavenDocker/settings.xml:/root/.m2/settings.xml:ro -v /d/software/maven/repository:/root/.m2/repository:rw -v /d/project/openSource/uid-service:/mnt/myproject:rw -w /mnt/myproject  vegardit/graalvm-maven:latest-java21 mvn clean -Pnative native:compile
 
 set name=uid-service
-set versionPrefix=V1.0.0
+set versionPrefix=v1.0.0
 
-set versionSuffix=-%date:~0,4%-%date:~5,2%-%date:~8,2%-RELEASE
-set version=%versionPrefix%%versionSuffix%
+set timeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%
 
-docker build -f Dockerfile -t activeclub/%name%:%version% -t activeclub/%name%:latest .
+set tagNative=activeclub/%name%:%versionPrefix%-native-%timeStr%
+set tagJvm=activeclub/%name%:%versionPrefix%-jvm-%timeStr%
+set tagLatest=activeclub/%name%:latest
 
-docker push activeclub/%name%:%version%
-docker push activeclub/%name%:latest
+docker build -f build/Dockerfile-native -t %tagNative% .
+docker build -f build/Dockerfile-jvm -t %tagJvm% -t %tagLatest% .
+
+docker push %tagNative%
+docker push %tagJvm%
+docker push %tagLatest%
